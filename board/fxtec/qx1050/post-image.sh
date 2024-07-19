44bc81acb499ded73aff32c88bdfecb70e4a0a41
Danct12 <danct12@disroot.org>
2024-07-19 10:47:59 +0000

    Add initial support for Qualcomm/Android boot images

--- board/fxtec/qx1050/post-image.sh
+++ board/fxtec/qx1050/post-image.sh
@@ -3,24 +3,31 @@
 set -e
 
 BOARD_DIR="$(dirname $0)"
-GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+DEVICEINFO_CFG="${BOARD_DIR}/deviceinfo"
 
-# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
-# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
-# space. We don't rely on genimage to build the rootfs image, just to insert a
-# pre-built one in the disk image.
+if [ ! -f ${DEVICEINFO_CFG} ]
+then
+	echo "${DEVICEINFO_CFG} doesn't exist. Cannot proceed."
+	exit 1
+fi
 
-trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
-ROOTPATH_TMP="$(mktemp -d)"
+# We adapt postmarketOS deviceinfo here because we like it. <3
+source ${DEVICEINFO_CFG}
 
-rm -rf "${GENIMAGE_TMP}"
+# Append DTB to compressed kernel image.
+# This is what Qualcomm bootloaders likes.
+cat ${BINARIES_DIR}/Image.gz ${BINARIES_DIR}/*.dtb > ${BINARIES_DIR}/Image.gz-dtb
 
-genimage \
-	--rootpath "${ROOTPATH_TMP}"   \
-	--tmppath "${GENIMAGE_TMP}"    \
-	--inputpath "${BINARIES_DIR}"  \
-	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+# Make a flashable Android boot image
+${HOST_DIR}/bin/mkbootimg \
+	--kernel ${BINARIES_DIR}/Image.gz-dtb \
+	--base "${deviceinfo_flash_offset_base}" \
+	--kernel_offset "${deviceinfo_flash_offset_kernel}" \
+	--ramdisk_offset "${deviceinfo_flash_offset_ramdisk}" \
+	--second_offset "${deviceinfo_flash_offset_second}" \
+	--tags_offset "${deviceinfo_flash_offset_tags}" \
+	--pagesize "${deviceinfo_flash_pagesize}" \
+	--cmdline "${deviceinfo_kernel_cmdline}" \
+	-o "${BINARIES_DIR}/boot.img"
 
 exit $?
